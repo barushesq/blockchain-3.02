9e13bfed84a97bab602a5da353ed7b8a66097b6f
barushesq <barushesq@gmail.com>
2024-03-07 13:10:30 -0600

    Update to working version

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import logo from "../../images/TEC1.png";
 const Footer = () => (
   <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
     <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
-      <div className="flex flex-[0.5] justify-center items-center">
+      <div className="flex flex -[0.5] justify-center items-center">
         <img src={logo} alt="logo" className="w-32" />
       </div>
       </div>
@@ -17,7 +17,7 @@ const Footer = () => (
     <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
     <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-      <p className="text-white text-left text-xs">Barush Esqueda</p>
+      <p className="text-white text-left text-xs">barushesq@gmail.com</p>
       
     </div>
   </div>
